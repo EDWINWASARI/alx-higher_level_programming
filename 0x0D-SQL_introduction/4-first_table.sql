405e56d09477af970a84437946d0bfe9fa5eab67
EDWINWASARI <edwinwasari@gmail.com>
2024-02-14 00:31:23 -0800

    a script that creates a table in MySQL server.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- This script  creates a table in the current database in MySQL server.
-CREATE DATABASE `first_table` (`id` INT, `name`, VARCHAR (256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name`, VARCHAR (256));
