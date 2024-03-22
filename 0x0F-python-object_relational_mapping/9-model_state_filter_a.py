b8cbb4c022e6d27397fe23795c214c351f6c0f7b
EDWINWASARI <edwinwasari@gmail.com>
2024-03-22 09:06:10 -0700

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-# Lists all State objects that contain the letter a
-# from the database hbtn_0e_6_usa.
-# Usage: ./9-model_state_filter_a.py <mysql username> /
-#                                    <mysql password> /
-#                                    <database name>
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from base import Base
 from model_state import State
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Check if the correct number of arguments is provided
+    if len(argv) != 4:
+        print("Usage: {} username password database".format(argv[0]))
+        exit(1)
+
+    # Database connection parameters
+    username, password, database = argv[1], argv[2], argv[3]
+    connection_string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database)
+
+    # Create an engine
+    engine = create_engine(connection_string, pool_pre_ping=True)
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
+    # Create database tables if they don't exist
+    Base.metadata.create_all(engine)
+
+    try:
+        # Query for all State objects and print their details
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
             print("{}: {}".format(state.id, state.name))
+    except Exception as e:
+        print("Error:", e)
+        session.rollback()  # Rollback changes in case of error
+    finally:
+        session.close()  # Close the session
