49b52e196d8063b80b5dbd98921808823c1059df
EDWINWASARI <edwinwasari@gmail.com>
2024-03-22 09:56:47 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-Script that lists all State objects from the database - Using module SQLAlchemy
-"""
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(argv) != 4:
-        print("Usage: {} username password database".format(argv[0]))
-        exit(1)
-
-    # Database connection parameters
-    username, password, database = argv[1], argv[2], argv[3]
-    connection_string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-        username, password, database)
-
-    # Create an engine
-    engine = create_engine(connection_string, pool_pre_ping=True)
-
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session
-    session = Session()
-
-    # Create database tables if they don't exist
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    try:
-        # Query for all State objects and print their details
-        states = session.query(State).order_by(State.id).all()
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-    except Exception as e:
-        print("Error:", e)
-        session.rollback()  # Rollback changes in case of error
-    finally:
-        session.close()  # Close the session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
