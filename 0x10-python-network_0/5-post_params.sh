b2f663f59a3d005e68afe8d329ead0ef2f3123be
EDWINWASARI <edwinwasari@gmail.com>
2024-03-27 20:10:00 -0700

    a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
+# takes in a URL, sends a POST request to the passed URL,
+curl -sX POST -d "email=hr@holbertonschool.com" \
+    -d "subject=I will always be here for PLD" \
+    "$1"
